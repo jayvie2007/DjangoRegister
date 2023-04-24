329cf99ab511d0e2a122206547ae2a33ae5f0d35
Jayvie De Leon <123736031+jayvie2007@users.noreply.github.com>
2023-04-24 18:00:12 +0800

    added register templates

--- authsystem/users/urls.py
+++ authsystem/users/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
+    path('register', views.register, name = "register"),
 ]
--- authsystem/users/views.py
+++ authsystem/users/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User, auth
 # Create your views here.
 
 def home(request):
     return render(request,"users/home.html")
+
+def register(request):
+    if request.method =='POST':
+        first_name = request.POST['first_name']
+        middle_name = request.POST['middle_name']
+        last_name = request.POST['last_name']
+        address = request.POST['address']
+        city = request.POST['city']
+        state = request.POST['state']
+        zip = request.POST['zip']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+    else:
+        return render(request, "register.html")
+        # if password == confirm_password:
+        #     if User.objects.filter(username=username):
+        
