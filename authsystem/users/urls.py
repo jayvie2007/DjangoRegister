37ba4f74f5ad98964fa10318eace2ebb23c54855
Jayvie De Leon <123736031+jayvie2007@users.noreply.github.com>
2023-04-26 11:19:55 +0800

    added minor changes

--- authsystem/users/urls.py
+++ authsystem/users/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
-    path('register', views.register, name = "register"),
-    path('login', views.login, name = "login")
+    path('register/', views.register, name = "register"),
+    path('login/', views.login, name = "login")
 ]
--- authsystem/users/views.py
+++ authsystem/users/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
+from django.contrib import messages
+from .models import User
 # Create your views here.
 
 def home(request):
@@ -18,8 +20,34 @@ def register(request):
         email = request.POST['email']
         password = request.POST['password']
         confirm_password = request.POST['confirm_password']
+        if password == confirm_password:
+            if User.objects.filter(username=username):
+                messages.info(request, 'Username already exist')
+                return redirect(register)
+            if User.objects.filter(email=email):
+                messages.info(request, 'Email already exist')
+                return redirect(register)
+            else: 
+                new_user = User.objects.create_user(
+                    first_name = first_name,
+                    middle_name = middle_name,
+                    last_name = last_name,
+                    address = address,
+                    city = city,
+                    state = state,
+                    zip = zip,
+                    email = email,
+                    password = password,
+                    confirm_password = confirm_password
+                )
+                new_user.set_password(password)
+                new_user.save()
+                return redirect('login')
+
+
     else:
-        return render(request, "register.html")
-        # if password == confirm_password:
-        #     if User.objects.filter(username=username):
+        return render(request, "users/register.html")
+        
+def login(request):
+    return render(request, "users/login.html")
         
